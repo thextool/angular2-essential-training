92b09e5b2a1674841f60bf6f29c9e4e22c25e0ba
thextool <matthew.riach@gmail.com>
2018-10-12 18:59:55 -0600

    :zap: added property binding and event binding

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: 'app/media-item.component.html',
   styleUrls: ['app/media-item.component.css']
 })
-export class MediaItemComponent { }
+export class MediaItemComponent {
+  @Input() mediaItem
+  
+  onDelete() {
+    console.log('delete')
+  }
+
+}
