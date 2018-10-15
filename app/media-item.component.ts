3060be07e31609bcbfb3eb87e03ecaf0b452a5b7
thextool <matthew.riach@gmail.com>
2018-10-14 18:04:24 -0600

    :zap: added the watched components

--- app/media-item.component.ts
+++ app/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['app/media-item.component.css']
 })
 export class MediaItemComponent {
+  @Input() mediaItem
+  @Output() delete = new EventEmitter()
+
   onDelete() {
-    console.log('deleted');
+    this.delete.emit(this.mediaItem)
   }
 }
